e93eb294fee0e24a29876355d43aefd098f2c359
cugucugu <cumhurgurleyen@gmail.com>
2025-06-14 16:17:05 +0700

    Update Plugin.kt

--- yenikaynak/src/main/kotlin/com/yenikaynak/Plugin.kt
+++ yenikaynak/src/main/kotlin/com/yenikaynak/Plugin.kt
@@ -8,8 +8,5 @@ import com.lagradost.cloudstream3.plugins.Plugin
 class YenikaynakPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(YenikaynakProvider())
-        registerExtractorPlugin(VidmolyExtractor())
-        registerExtractorPlugin(DoodStreamExtractor())
-        registerExtractorPlugin(StreamTapeExtractor())
     }
 }
