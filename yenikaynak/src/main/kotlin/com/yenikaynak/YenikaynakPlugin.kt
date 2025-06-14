5d92e346a26cb9f6a974b6431d9391cd31fca671
cugucugu <cumhurgurleyen@gmail.com>
2025-06-14 18:04:49 +0700

    Update and rename Plugin.kt to YenikaynakPlugin.kt

---
+++ yenikaynak/src/main/kotlin/com/yenikaynak/YenikaynakPlugin.kt
@@ -0,0 +1,14 @@
+package com.lagradost.cloudstream3.plugins
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class YenikaynakPlugin: Plugin() {
+    override fun load(context: Context) {
+        // Bu fonksiyon, eklentinin ana sınıfını CloudStream'e kaydeder.
+        // Bu sayede uygulama, "Yenikaynak" adında yeni bir kaynak olduğunu anlar.
+        registerMainAPI(Yenikaynak())
+    }
+}
