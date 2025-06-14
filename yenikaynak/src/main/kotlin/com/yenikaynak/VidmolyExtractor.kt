5f870d5584330ba896eb1aae29aecc50a1fa79e4
cugucugu <cumhurgurleyen@gmail.com>
2025-06-14 16:39:40 +0700

    Update VidmolyExtractor.kt

--- yenikaynak/src/main/kotlin/com/yenikaynak/VidmolyExtractor.kt
+++ yenikaynak/src/main/kotlin/com/yenikaynak/VidmolyExtractor.kt
@@ -1,7 +1,6 @@
 package com.yenikaynak
 
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.extractors.*
 import com.lagradost.cloudstream3.utils.*
 
 class VidmolyExtractor : ExtractorApi() {
@@ -12,19 +11,18 @@ class VidmolyExtractor : ExtractorApi() {
     override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink>? {
         val headers = mapOf("Referer" to (referer ?: mainUrl))
         val document = app.get(url, headers = headers).document
+
         val script = document.selectFirst("script:containsData(sources: [)")?.data() ?: return null
         val match = Regex("""file:\s*["'](https?[^"']+\.mp4)["']""").find(script) ?: return null
         val videoUrl = match.groupValues[1]
 
-        return listOf(
-            newExtractorLink(
-                name = name,
-                source = name,
-                url = videoUrl,
-                referer = referer ?: mainUrl,
-                quality = Qualities.Unknown.value,
-                isM3u8 = false
-            )
-        )
+        return listOf(newExtractorLink {
+            this.name = this@VidmolyExtractor.name
+            this.source = this@VidmolyExtractor.name
+            this.url = videoUrl
+            this.referer = referer ?: mainUrl
+            this.quality = Qualities.Unknown.value
+            this.isM3u8 = false
+        })
     }
 }
