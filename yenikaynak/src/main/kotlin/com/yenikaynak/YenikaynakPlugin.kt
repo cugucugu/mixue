37257941c99b1fdc3d0d31e2794bdd16d4cde30b
cugucugu <cumhurgurleyen@gmail.com>
2025-06-14 18:14:03 +0700

    Update YenikaynakPlugin.kt

--- yenikaynak/src/main/kotlin/com/yenikaynak/YenikaynakPlugin.kt
+++ yenikaynak/src/main/kotlin/com/yenikaynak/YenikaynakPlugin.kt
@@ -7,8 +7,6 @@ import android.content.Context
 @CloudstreamPlugin
 class YenikaynakPlugin: Plugin() {
     override fun load(context: Context) {
-        // Bu fonksiyon, eklentinin ana sınıfını CloudStream'e kaydeder.
-        // Bu sayede uygulama, "Yenikaynak" adında yeni bir kaynak olduğunu anlar.
         registerMainAPI(Yenikaynak())
     }
 }
