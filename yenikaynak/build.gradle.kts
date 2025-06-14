c5b9e8878026461aafcff5abccbf264991b3fdf9
cugucugu <cumhurgurleyen@gmail.com>
2025-06-14 16:29:07 +0700

    Update build.gradle.kts

--- yenikaynak/build.gradle.kts
+++ yenikaynak/build.gradle.kts
@@ -4,7 +4,7 @@ version = 1
 cloudstream {
     language = "tr"
     description = "Yenikaynak.com içerik sağlayıcısı"
-    authors = listOf("Yenikaynak")
+    authors = listOf("Cugucugu")
 
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("Movies")
