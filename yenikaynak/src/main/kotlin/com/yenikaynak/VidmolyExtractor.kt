b5122beaad25e42b0b6bea8adb33c0aeb0059c15
cugucugu <cumhurgurleyen@gmail.com>
2025-06-14 16:28:28 +0700

    Update VidmolyExtractor.kt

--- yenikaynak/src/main/kotlin/com/yenikaynak/VidmolyExtractor.kt
+++ yenikaynak/src/main/kotlin/com/yenikaynak/VidmolyExtractor.kt
@@ -1,10 +1,8 @@
-package com.cugucugu.yenikaynak
+package com.yenikaynak
 
-import android.util.Log
-import org.jsoup.nodes.Element
 import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.extractors.*
 import com.lagradost.cloudstream3.utils.*
-import kotlin.random.Random
 
 class VidmolyExtractor : ExtractorApi() {
     override val name = "Vidmoly"
@@ -14,13 +12,12 @@ class VidmolyExtractor : ExtractorApi() {
     override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink>? {
         val headers = mapOf("Referer" to (referer ?: mainUrl))
         val document = app.get(url, headers = headers).document
-
         val script = document.selectFirst("script:containsData(sources: [)")?.data() ?: return null
         val match = Regex("""file:\s*["'](https?[^"']+\.mp4)["']""").find(script) ?: return null
         val videoUrl = match.groupValues[1]
 
         return listOf(
-            ExtractorLink(
+            newExtractorLink(
                 name = name,
                 source = name,
                 url = videoUrl,
